b9909c938cc8d178726fcc1254f4e0e224aa2e79
Boris Yakimov <boris.yakimov@itgix.com>
2026-01-21 15:03:24 +0200

    update lambda runtime to python 3.14

--- lambda.tf
+++ lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "log_retention_enforcer" {
   function_name = "cloudwatch-log-retention-enforcer"
   role          = aws_iam_role.lambda_role.arn
   handler       = "retention_enforcer.lambda_handler"
-  runtime       = "python3.10"
+  runtime       = "python3.14"
 
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
